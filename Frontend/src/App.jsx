e13da6178eb7545cea8473764df849345d5c7654
sapinder5911-create <sapinder5911@gmail.com>
2026-03-15 15:08:50 +0530

    add url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,7 @@ import { Toaster } from "react-hot-toast";
 import OrderDetails from "./Component/OderDetailPage"
 // import { useState } from 'react'
 import './App.css'
-import { div, form, pre } from 'framer-motion/client'
+// import { div, form, pre } from 'framer-motion/client'
 import AdminProtectedRoute from "./Component/AdminProtectedRoute";
 import AdminLayout from './Component/Adminlayout'
 import Deleteproduct from "./Component/Deletproduct"
