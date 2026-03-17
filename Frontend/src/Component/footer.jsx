9922c4e2aef3b8df40c07760e91563431f5768a2
sapinder5911-create <sapinder5911@gmail.com>
2026-03-17 14:44:25 +0530

    add data

--- Frontend/src/Component/footer.jsx
+++ Frontend/src/Component/footer.jsx
@@ -71,6 +71,84 @@
 // export default Footer
 
 
+// import { motion } from "framer-motion";
+
+// function Footer() {
+//     return (
+//         <>
+
+//             <div className="min-h-[360px] w-full flex items-center justify-center bg-black border border-white">
+
+//                 <motion.div
+//                     className="w-full max-w-[1400px] flex flex-wrap md:flex-nowrap justify-evenly gap-8 p-6 bg-black mt-5"
+//                     initial={{ opacity: 0, y: 100 }}
+//                     whileInView={{ opacity: 1, y: 0 }}
+//                     transition={{ duration: 1 }}
+//                     viewport={{ once: true, amount: 0.2 }}
+//                 >
+
+//                     <div className="w-full sm:w-1/2 md:w-1/4 text-white font-serif font-bold">
+//                         <p className="text-2xl">Contect</p>
+
+//                         Address: 500 Terry <br />
+//                         Francine Street <br />
+//                         San Francisco, CA 94158 <br />
+//                         Phone: 123-456-7890 <br />
+//                         Email: info@mysite.com <br />
+//                     </div>
+
+//                     <div className="w-full sm:w-1/2 md:w-1/4 text-white font-serif font-bold">
+//                         <p className="text-2xl">Shop</p>
+
+//                         Shop All Plants <br />
+//                         Pots <br />
+//                         Sale <br />
+//                         Subscriptions <br />
+//                     </div>
+
+//                     <div className="w-full sm:w-1/2 md:w-1/4 text-white font-serif font-bold">
+//                         <p className="text-2xl">helpful Links</p>
+
+//                         FAQ <br />
+//                         Shipping Policy <br />
+//                         Refund Policy <br />
+//                         Terms 8 Conditions <br />
+//                         Accessibility Statement <br />
+//                         Privacy Policy <br />
+//                     </div>
+
+//                     <div className="w-full sm:w-1/2 md:w-1/4 text-white font-serif font-bold">
+//                         <p className="text-2xl">Company</p>
+
+//                         Our Story <br />
+//                         Contact Us <br />
+//                     </div>
+
+//                 </motion.div>
+
+//             </div>
+
+
+//             <div className="h-20 w-full border border-black flex bg-black">
+
+//                 <motion.p
+//                     className="text-white text-center text-sm font-serif font-bold m-auto px-4"
+//                     initial={{ opacity: 0, y: 100 }}
+//                     whileInView={{ opacity: 1, y: 0 }}
+//                     transition={{ duration: 1 }}
+//                     viewport={{ once: true, amount: 0.2 }}
+//                 >
+//                     Copyright © 2026 Plants's Co. All rights reserved.
+//                 </motion.p>
+
+//             </div>
+
+//         </>
+//     );
+// }
+
+// export default Footer;
+
 import { motion } from "framer-motion";
 
 function Footer() {
@@ -80,7 +158,7 @@ function Footer() {
             <div className="min-h-[360px] w-full flex items-center justify-center bg-black border border-white">
 
                 <motion.div
-                    className="w-full max-w-[1400px] flex flex-wrap md:flex-nowrap justify-evenly gap-8 p-6 bg-black mt-5"
+                    className="w-full flex flex-wrap md:flex-nowrap justify-evenly gap-8 p-6 bg-black mt-5"
                     initial={{ opacity: 0, y: 100 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1 }}
