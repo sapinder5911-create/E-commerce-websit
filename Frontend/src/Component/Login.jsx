629abbd99afafb8e856ce003919074556e7ddf46
sapinder5911-create <sapinder5911@gmail.com>
2026-03-17 17:32:44 +0530

    add data

--- Frontend/src/Component/Login.jsx
+++ Frontend/src/Component/Login.jsx
@@ -214,7 +214,7 @@ function Login() {
             >
                 <div className="max-w-xl">
 
-                    <h1 className=" text-2xl sm:text-3xl md:text-5xl  font-serif   font-bold
+                    <h1 className=" text-2xl sm:text-3xl md:text-5xl  font-serif   font-bold ml-20
                     leading-tight
                     ">
                         Make Your Home a Greener Place
@@ -225,7 +225,7 @@ function Login() {
 
             {/* RIGHT FORM SECTION */}
             <motion.div
-                className="  md:w-1/2  flex items-center    justify-center  p-4    sm:p-6
+                className="  md:w-1/3 flex items-center justify-center p-6 
                 "
                 initial={{ opacity: 0, x: 100 }}
                 animate={{ opacity: 1, x: 0 }}
@@ -235,18 +235,7 @@ function Login() {
                 <form
                     onSubmit={handleSubmit(hello)}
                     className="  bg-white bg-opacity-90  backdrop-blur-md shadow-2xl rounded-2xl  p-6 sm:p-8   w-full  flex   flex-col gap-5
-                    "
-                    
-                   
-                    
-                    
-                   
-                    
-                  
-                  
-                    
-                >
-
+                    ">
                     <h2 className="text-xl sm:text-2xl font-semibold text-center mb-2">
                         Login
                     </h2>
@@ -311,7 +300,7 @@ function Login() {
                     <div className="flex justify-between gap-4 mt-3">
 
                         <Link
-                            to="/"
+                            to="/" id="hello"
                             className=" flex-1  text-center  text-white py-2 rounded-xl transition
                             "
                         >
@@ -320,7 +309,7 @@ function Login() {
 
                         <button
                             type="submit"
-                            className="  flex-1   bg-pink-700  text-black py-2  rounded-xl  hover:border-pink-800  transition  border
+                            className="  flex-1   text-blue  text-black py-2  rounded-xl  hover:border-pink-800  transition  border
                             border-black
                             "
                         >
