de09a3780aecd169558bd0b3d819305db650923d
sapinder5911-create <sapinder5911@gmail.com>
2026-03-17 14:26:43 +0530

    add data

--- Frontend/src/Component/Nav.jsx
+++ Frontend/src/Component/Nav.jsx
@@ -311,7 +311,7 @@ function Nav() {
 
     return (
 
-        <div className="h-20 w-full flex items-center justify-center bg-[url(https://static.wixstatic.com/media/c837a6_35bdfabce3724dad92a9f8828437078e~mv2.jpeg/v1/fill/w_1873,h_945,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_85e5f4e9599f470cb22efaa7e0cf7d8f~mv2.jpeg)] bg-cover">
+        <div className="h-20 w-380 flex items-center justify-center bg-[url(https://static.wixstatic.com/media/c837a6_35bdfabce3724dad92a9f8828437078e~mv2.jpeg/v1/fill/w_1873,h_945,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_85e5f4e9599f470cb22efaa7e0cf7d8f~mv2.jpeg)] bg-cover">
 
             <div className="h-20 w-full max-w-[1400px] px-4 flex items-center justify-between text-xl">
 
